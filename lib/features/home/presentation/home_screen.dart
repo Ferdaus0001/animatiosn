4a13ef46198de1d3630a9461994266ddf0f29502
Ferdaus0001 <ferdaushossan7788990@gmail.com>
2025-01-17 15:08:46 +0600

    ferdaus

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -7,21 +7,41 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(seconds: 3),
+  )..repeat(reverse: true);
+
+  late final Animation<Offset> _animationOffset = Tween<Offset>(
+    begin: Offset.zero,
+    end: const Offset(0.1, 0.4),
+  ).animate(_controller);
+
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(padding: EdgeInsets.symmetric(vertical: 22,horizontal: 22),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 22, horizontal: 22),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-
+            SlideTransition(
+              position: _animationOffset,
+              child:  CircleAvatar( backgroundImage: AssetImage
+                ('assets/pexels-photo-1199957.webp'),radius: 50,),
+            ),
           ],
         ),
       ),
     );
+  }
 
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 }
